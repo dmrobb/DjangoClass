e516f9ae4f2cb75ef83f3d940b81dff52fcf3aa3
dmrobb <59259629+dmrobb@users.noreply.github.com>
2022-06-08 22:08:58 -0400

    session 99 complete

--- rwr_site-project/classroom/urls.py
+++ rwr_site-project/classroom/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import HomeView, ThankYouView, ContactFormView
+from .views import (HomeView, ThankYouView,
+                    ContactFormView,TeacherCreateView,
+                    TeacherListView,TeacherDetailView,
+                    TeacherUpdateView)
+
 
 app_name = 'classroom'
 
@@ -7,4 +11,9 @@ urlpatterns = [
     path('',HomeView.as_view(),name='home'), #path expects a funcion
     path('thank_you/',ThankYouView.as_view(),name="thank_you"),
     path('contact/',ContactFormView.as_view(),name="contact"),
+    path('create_teacher/',TeacherCreateView.as_view(),name='create_teacher'),
+    path('list_teacher/',TeacherListView.as_view(),name='list_teacher'),
+    path('teacher_detail/<int:pk>',TeacherDetailView.as_view(),name='detail_teacher'),
+    path('update_teacher/<int:pk>',TeacherUpdateView.as_view(),name='update_teacher'),
+
 ]
--- rwr_site-project/classroom/views.py
+++ rwr_site-project/classroom/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse,reverse_lazy
-from django.views.generic import TemplateView,FormView
+from django.views.generic import (TemplateView,FormView,CreateView,
+                                    ListView,DetailView,UpdateView)
 from classroom.forms import ContactForm
-
+from classroom.models import Teacher
 
 # Create your views here.
 # def home_view(request):
@@ -13,6 +14,27 @@ class HomeView(TemplateView):
 
 class ThankYouView(TemplateView):
     template_name = 'classroom/thank_you.html'
+
+class TeacherCreateView(CreateView):
+    model = Teacher
+    fields = '__all__'
+    success_url = reverse_lazy('classroom:thank_you')
+
+class TeacherListView(ListView):
+    model = Teacher
+     # optional to order or filter results
+    queryset = Teacher.objects.order_by('first_name')
+    context_object_name = 'teacher_list'
+
+class TeacherDetailView(DetailView):
+    model = Teacher
+
+class TeacherUpdateView(UpdateView):
+    model = Teacher
+    fields = '__all__'
+    # fields = ['last_name', 'first_name']
+    success_url = reverse_lazy('classroom:list_teacher')
+
 
 class ContactFormView(FormView):
     form_class = ContactForm
